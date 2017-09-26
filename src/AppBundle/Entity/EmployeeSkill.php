f86f206a7fdc60483a9c1cefcb70b41f1e37c5e4
Eduardo Pieretti Umpierre <eduardoumpierre@hotmail.com>
2017-09-26 12:27:56 -0300

    EmployeeSkill entity created

---
+++ src/AppBundle/Entity/EmployeeSkill.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * EmployeeSkill
+ *
+ * @ORM\Table(name="employees_skills")
+ * @ORM\HasLifecycleCallbacks()
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\EmployeeSkillRepository")
+ */
+class EmployeeSkill
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Employee")
+     * @ORM\JoinColumn(name="id_employees", referencedColumnName="id")
+     */
+    private $employee;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Skill")
+     * @ORM\JoinColumn(name="id_skills", referencedColumnName="id")
+     */
+    private $skill;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime", nullable=true)
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updated_at", type="datetime", nullable=true)
+     */
+    private $updatedAt;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get employee
+     *
+     * @return Employee
+     */
+    public function getEmployee()
+    {
+        return $this->employee;
+    }
+
+    /**
+     * Set employee
+     *
+     * @param Employee $employee
+     *
+     * @return $this
+     */
+    public function setEmployee(Employee $employee)
+    {
+        $this->employee = $employee;
+
+        return $this;
+    }
+
+    /**
+     * Set skill
+     *
+     * @return Skill
+     */
+    public function getSkill()
+    {
+        return $this->skill;
+    }
+
+    /**
+     * Set skill
+     *
+     * @param Skill $skill
+     *
+     * @return $this
+     */
+    public function setSkill(Skill $skill)
+    {
+        $this->skill = $skill;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function setCreated()
+    {
+        $this->createdAt = new \DateTime('NOW', new \DateTimeZone('America/Sao_Paulo'));
+    }
+
+    /**
+     * @ORM\PrePersist
+     * @ORM\PreUpdate
+     */
+    public function setUpdated()
+    {
+        $this->updatedAt = new \DateTime('NOW', new \DateTimeZone('America/Sao_Paulo'));
+    }
+}
+

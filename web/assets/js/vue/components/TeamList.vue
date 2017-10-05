84df9f6e80d97c47435c38d5252b072e3e90fe33
Eduardo Pieretti Umpierre <eduardoumpierre@hotmail.com>
2017-10-05 18:19:45 -0300

    Team list component added

---
+++ web/assets/js/vue/components/TeamList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="row">
+        <div
+                class="col-xs-12 col-sm-4 tr_eq"
+                v-for="person in team">
+            <h3>{{ person.name }}</h3>
+
+            <ul>
+                <li v-for="skill in person.skills">
+                    {{ skill.name }} - {{ skill.level }}
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'team-list',
+        props: ['team']
+    }
+</script>

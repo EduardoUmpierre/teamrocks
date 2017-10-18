0aca8af35f62924266c5f8d9875d1d775f868ea6
Eduardo Pieretti Umpierre <eduardoumpierre@hotmail.com>
2017-10-18 13:29:55 -0200

    Task list component created

---
+++ web/assets/js/vue/components/TaskList.vue
@@ -0,0 +1,17 @@
+<template>
+    <ol class="row">
+        <li
+                class="col-xs-12 col-sm-4 tr_eq"
+                v-for="task in tasks">
+            <h3>{{ task.title }} - {{ task.skill }}</h3>
+            <p>{{ task.description }}</p>
+        </li>
+    </ol>
+</template>
+
+<script>
+    export default {
+        name: 'task-list',
+        props: ['tasks']
+    }
+</script>

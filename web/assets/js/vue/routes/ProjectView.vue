3e774c3698d77afda13a8a9ab5c58e74c4953780
Eduardo Pieretti Umpierre <eduardoumpierre@hotmail.com>
2017-12-05 16:21:53 -0200

    Project title tag updated

--- web/assets/js/vue/routes/ProjectView.vue
+++ web/assets/js/vue/routes/ProjectView.vue
@@ -1,23 +1,38 @@
 <template>
-    <div>
-        <div v-if="project">
-            <main class="tr_content_home">
-                <div class="container">
+    <main>
+        <div class="container" v-if="project">
+            <div class="row">
+                <div class="col-xs-12">
                     <h2>Projeto</h2>
 
-                    <h3>{{ project.title }}</h3>
+                    <h1>{{ project.title }}</h1>
                     <p>{{ project.description }}</p>
+                </div>
+            </div>
 
+            <div class="row">
+                <div class="col-xs-12">
                     <h2>Equipe</h2>
                     <team-list v-bind:team="project.team"></team-list>
+                </div>
+            </div>
 
+            <div class="row">
+                <div class="col-xs-12">
                     <h2>Tasks</h2>
                     <task-list v-bind:tasks="project.tasks"></task-list>
                 </div>
-            </main>
+            </div>
         </div>
-    </div>
+    </main>
 </template>
+
+<style lang="scss" scoped>
+    @import '../../../scss/helpers/flexbox';
+    @import '../../../scss/helpers/mixins';
+    @import '../../../scss/components/buttons';
+    @import '../../../scss/components/list';
+</style>
 
 <script>
     import TeamList from '../components/TeamList.vue'

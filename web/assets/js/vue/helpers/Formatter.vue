228cff0da70c1647d22d5db4edf731c6f13e23b7
Eduardo Pieretti Umpierre <eduardoumpierre@hotmail.com>
2017-12-06 16:06:23 -0200

    Formatter helper added

---
+++ web/assets/js/vue/helpers/Formatter.vue
@@ -0,0 +1,30 @@
+<template>
+    <span>{{ formatter[fn](value, format) }}</span>
+</template>
+
+<script>
+    import moment from 'moment'
+
+    export default {
+        name: 'formatter',
+        props: ['value', 'fn', 'format'],
+        computed: {
+            formatter: function() {
+                var formatter = {
+                    date: function (value, format) {
+                        if (value) {
+                            return moment(String(value)).format(format || 'DD/MM/YYYY')
+                        }
+                    },
+                    time: function (value, format) {
+                        if (value) {
+                            return moment(String(value)).format(format || 'h:mm A');
+                        }
+                    }
+                };
+
+                return formatter;
+            }
+        }
+    }
+</script>
